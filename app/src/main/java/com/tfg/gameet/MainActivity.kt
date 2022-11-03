6cfe62b39dff305d7c3a344d70197743ba87b1da
Alesancab1 <alesancab@alum.us.es>
2022-11-03 19:44:39 +0100

    INTEGRACION FIREBASE
    Modificación de ficheros build.gradle para integrar con firebase y el funcionamiento de la base de datos Firestore
    
    Sanabria Cabeza, Alejandro <@alesancab1>

--- app/src/main/java/com/tfg/gameet/MainActivity.kt
+++ app/src/main/java/com/tfg/gameet/MainActivity.kt
@@ -2,10 +2,22 @@ package com.tfg.gameet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Splash Screen
+        Thread.sleep(2000)
+        setContentView(R.layout.activity_main)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Analytics Event
+        val analytics : FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integración de Firebase completa")
+        analytics.logEvent("InitScreen", bundle)
     }
 }
